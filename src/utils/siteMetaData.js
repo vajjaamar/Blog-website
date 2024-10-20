0d227d62dd896aca13377ee64080c49b28c691f1
vajjaamar <amarnathvajja8074@gmail.com>
2024-10-20 11:37:43 +0530

    Add files via upload

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'CodeBucks',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'codebucks27@gmail.com', 
+    github: 'https://github.com/aura-01',
+    twitter: 'https://x.com/Amruthach01',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com/in/amrutha-ch/',
+    dribbble: 'https://dribble.com/aura-01',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
